8498047454addb7bbcec9084ee6e38d539180b38
nixthepup <34139211+nixthepup@users.noreply.github.com>
2018-03-19 12:18:29 -0500

    Create RetailItem.java

---
+++ RetailItem.java
@@ -0,0 +1,42 @@
+public class RetailItem
+{
+	private String description;
+	private int unitsOnHand;
+	private double price;
+	
+	public RetailItem(String d, int u, double p)
+	{
+		description = d;
+		unitsOnHand = u;
+		price = p;
+	}
+	public void setDescription(String d)
+	{
+		description = d;
+	}
+
+	public void setUnitsOnHand(int u)
+	{
+		unitsOnHand = u;
+	}
+	
+	public void setPrice(double p)
+	{
+		price = p;
+	}
+	
+	public String getDescription()
+	{
+		return description;
+	}
+
+	public int getUnitsOnHand()
+	{
+		return unitsOnHand;
+	}
+	
+	public double getPrice()
+	{
+		return price;
+	}
+}
