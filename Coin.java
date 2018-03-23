5783fe958a80bb93a5e2651194a84013674f8f6f
nixthepup <34139211+nixthepup@users.noreply.github.com>
2018-03-23 14:10:41 -0500

    Add files via upload

---
+++ Coin.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Coin{
+private String sideUp ="";
+
+public Coin(){
+	Random random = new Random();
+	int randomnum;
+	randomnum = random.nexInt(2);
+	
+	
+	
+	
+	
---
+++ Cointdemo.java
@@ -0,0 +1,8 @@
+public class Cointdemo{
+public static void main (String[] args){
+Coin tossed = new Coin();
+System.out.println(tossed.getSideUp());
+
+
+
+}}
